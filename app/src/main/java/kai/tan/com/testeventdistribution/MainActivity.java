9568a038e4e071d08d1ad1c5113f21a3baea6e5e
luobodidi <1067629251@qq.com>
2017-04-07 18:52:52 +0800

    2017年4月7日18:52:46

--- app/src/main/java/kai/tan/com/testeventdistribution/MainActivity.java
+++ app/src/main/java/kai/tan/com/testeventdistribution/MainActivity.java
@@ -2,12 +2,35 @@ package kai.tan.com.testeventdistribution;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: cc");
+            }
+        });
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("TAG", "onTouch execute, action " + event.getAction());
+                //如果返回true, 会先执行onTouch然后执行onclick
+                //如果返回false，只会执行onTouch
+                return true;
+            }
+        });
     }
 }
