4e785ef39f75e402f22329e3648cf838b40347a6
luobodidi <1067629251@qq.com>
2017-07-28 15:05:22 +0800

    2017年7月28日15:05:16

--- app/src/main/java/kai/tan/com/testeventdistribution/view/MyLayout.java
+++ app/src/main/java/kai/tan/com/testeventdistribution/view/MyLayout.java
@@ -8,6 +8,7 @@ import android.widget.LinearLayout;
 
 /**
  * 理解onInterceptTouchEvent 拦截和不拦截在同一事件序列中的调用次数
+ * 拦截只会调用一次，不拦截每次事件都会被调用
  */
 public class MyLayout extends LinearLayout {
 
@@ -34,6 +35,7 @@ public class MyLayout extends LinearLayout {
         boolean b = super.onInterceptTouchEvent(ev);
         Log.d(TAG, "viewgroup-onInterceptTouchEvent-return: " + ev.getAction() + "-" + b);
         return b;
+
 //        return true;
         /*
          return true ：拦截事件
@@ -41,6 +43,22 @@ public class MyLayout extends LinearLayout {
          2 如果当前view拦截了某个事件，那么在同一个事件序列（手指接触屏幕那一刻到手指离开屏幕的那一刻）此方法不会再次被调用。
               比如按下的时候拦截了，移动和抬起事件时就不会再调用这个方法。
           */
+
+
+        /*
+         拦截move事件
+         button dispatchTouchEvent 出现cancel事件，有时间在深入。
+         */
+//        boolean b;
+//        if(ev.getAction() == MotionEvent.ACTION_MOVE) {
+//            b = true;
+//        }else {
+//            b = super.onInterceptTouchEvent(ev);
+//        }
+//        Log.d(TAG, "viewgroup-onInterceptTouchEvent-return: " + ev.getAction() + "-" + b);
+//        return b;
+
+
     }
 
     @Override
